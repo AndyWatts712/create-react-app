ca83bf8500f6c8eac07719c95fa3c0a194395914
megawatts68 <andrew.f.watts@gmail.com>
2020-08-21 00:06:20 +0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -3,28 +3,13 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  
+  
   return (
     <main>
       <h1>Create React App + Go API</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
+        Andy Watts
       </h2>
       <p>
         <a
@@ -48,8 +33,7 @@ function App() {
         .
       </p>
       <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      
     </main>
   );
 }
